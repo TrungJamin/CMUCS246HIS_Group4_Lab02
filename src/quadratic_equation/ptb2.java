6680f5604bf93e4b35e795b467659f4dbb2035c0
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2021-04-18 17:14:44 +0700

    update file ptb2.java

--- src/quadratic_equation/ptb2.java
+++ src/quadratic_equation/ptb2.java
@@ -5,6 +5,6 @@ public class ptb2 {
 	    public static void main(String[] args) {
 	        ptb2View view = new ptb2View();
 	        ptb2Controller controller = new ptb2Controller(view);
-	        System.out.println("Test code");
+	        System.out.println("Hello World");
 	    }
 }
